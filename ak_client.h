999005c0c4d1c24c951cf2068587c0565745a86b
User <user@localhost>
2020-11-15 16:39:10 +0300

    add ak_[server|client]

---
+++ ak_client.h
@@ -0,0 +1,6 @@
+#ifndef _PTY_SERVER_H_
+#define _PTY_SERVER_H_
+
+int spawn_pty_server(ak_fiot fctx);
+
+#endif // _PTY_SERVER_H_
---
+++ ak_server.h
@@ -0,0 +1,6 @@
+#ifndef _PTY_CLIENT_H_
+#define _PTY_CLIENT_H_
+
+int spawn_pty_client(ak_fiot fctx);
+
+#endif // _PTY_CLIENT_H_
