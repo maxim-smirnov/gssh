2782a63252ebc686435fa137ca709cd496fc1aa3
User <user@localhost>
2021-01-13 18:48:45 +0300

    fix: buffer size and timeot

--- config.h
+++ config.h
@@ -1,7 +1,7 @@
 #ifndef _CONFIG_H_
 #define _CONFIG_H_
 
-#define DATA_MAX_SIZE    2048
-#define COMMAND_TIMEOUT  60     /* timeout for bc connection */
+#define DATA_MAX_SIZE    1024
+#define COMMAND_TIMEOUT  300     /* timeout for bc connection */
 
 #endif // _CONFIG_H_
